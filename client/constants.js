ea75f93a21b5ee09efd8e961963cb440bed58aa9
DivyaMeghana7686 <divyamegha25@gmail.com>
2024-04-22 00:26:37 +0530

    Extrs deleted

--- client/constants.js
+++
@@ -1,4 +0,0 @@
-const Google_API_KEY="AIzaSyDzsl-ddnpyUPNRuPB_XlCpCWYsR8kPju0";
-
-export const YouTube_API_Key="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&key="+
-                Google_API_KEY;
